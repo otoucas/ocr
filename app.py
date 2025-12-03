df854e8072a9232908dfd21ce9b06314cfb5e914
otoucas <97524845+otoucas@users.noreply.github.com>
2025-12-03 16:18:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ def extract_text_from_image(image):
     api_url = "https://api.ocr.space/parse/image"
     payload = {
         'isOverlayRequired': False,
-        'apikey': 'helloworld',  # Clé API gratuite pour les tests
-        'language': 'fr',  # Utiliser 'fr' pour le français
+        'apkeyp': 'helloworld',  # Clé API gratuite pour les tests
+        'language': 'fra',  # Utiliser 'fra' pour le français
     }
 
     img_byte_arr = io.BytesIO()
     image.save(img_byte_arr, format='PNG')
     img_byte_arr = img_byte_arr.getvalue()
 
-    files = {'file': ('image.png', img_byte_arr, 'image/png')}  # Spécifier le nom et le type de fichier
+    files = {'file': ('image.png', img_byte_arr, 'image/png')}
     response = requests.post(api_url, files=files, data=payload)
     result = response.json()
 
